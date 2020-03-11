b3e409cc33dd1860dc587aa6bc85c76c9db0dbda
Andrei Oliveira <andrei.oliveira@guiabolso.com.br>
2020-03-11 14:11:55 -0300

    Fixed vars

--- variables.tf
+++ variables.tf
@@ -1,23 +1,29 @@
 variable "vpcName" {
   description = "Name of the VPC used"
+  default = "oclim-terraform"
 }
 
 variable "subnet1" {
   description = "Name of the f used"
+  default = "oclim-terraform-publicsubnet-1"
 }
 
 variable "subnet2" {
   description = "Name of the VPC used"
+  default = "oclim-terraform-publicsubnet-2"
 }
 
 variable "internal_security_group" {
   description = "Name of the f used"
+  default = "oclim-terraform-InternalSG"
 }
 
 variable "external_security_group" {
   description = "Name of the VPC used"
+  default = "oclim-terraform-externalSG"
 }
 
 variable "bucketName" {
   description = "Name of the VPC used"
+  default = "oclim-terraform-s3-bucket"
 }
